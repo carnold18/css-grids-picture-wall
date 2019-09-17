994b6d5ac6f51f44a4cdf341b59c7ef7fb4f1b8c
github username <github email address>
2019-09-16 20:19:58 -0700

    initial pass at creating img tags complete

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function fetchPictures() {
 
 function createDivs(data) {
     for (item of data) {
-        console.log(item["download_url"]);
+        // console.log(item["download_url"]);
+        img = document.createElement('img');
+        pictureDiv.appendChild(img);
+        img.src = item["download_url"];
+        img.alt = " "
+        console.log(img);
     }
 }
 
